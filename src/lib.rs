4e715beef8dbfe3c605dc626beb67ef0f805d37a
Greg Brown <greg.brown.00@outlook.com>
2020-03-12 13:52:15 +0000

    Add skeleton to i2cwrite.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use embedded_hal::blocking::i2c::{self, Read};
+use std::io;
+
+enum Encoding {
+    Identity,
+}
+
+struct I2cWriter<T: Read + i2c::Write> {
+    encoding: Encoding,
+    writer: T,
+}
+
+impl<T: Read + i2c::Write> io::Write for I2cWriter<T> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        unimplemented!()
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        unimplemented!()
     }
 }
